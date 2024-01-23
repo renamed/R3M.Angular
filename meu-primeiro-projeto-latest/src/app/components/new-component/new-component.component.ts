2f066642457533aceae6ebf81cc05d9051568342
Renato <renamedrj@gmail.com>
2024-01-23 16:01:45 -0300

    Class 16. Final

--- meu-primeiro-projeto-latest/src/app/components/new-component/new-component.component.ts
+++ meu-primeiro-projeto-latest/src/app/components/new-component/new-component.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { TesteComponent } from "../teste/teste.component";
 
 @Component({
-  selector: 'app-new-component',
-  standalone: true,
-  imports: [],
-  templateUrl: './new-component.component.html',
-  styleUrl: './new-component.component.scss'
+    selector: 'app-new-component',
+    standalone: true,
+    templateUrl: './new-component.component.html',
+    styleUrl: './new-component.component.scss',
+    imports: [TesteComponent]
 })
 export class NewComponent  {
 
