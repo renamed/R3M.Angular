b578f58e8e84ccfd80c957699af8e97265ec3de4
Renato <renamedrj@gmail.com>
2024-01-23 15:53:17 -0300

    Class 15. Criando um componente

--- meu-primeiro-projeto-latest/src/app/app.component.ts
+++ meu-primeiro-projeto-latest/src/app/app.component.ts
@@ -1,15 +1,16 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NewComponent } from './components/new-component/new-component.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, NewComponent],
   template: `
     <h1>Curso de Angular</h1>
-    <h2>Components</h2>
     <!-- <router-outlet></router-outlet> -->
+    <app-new-component />
     `
 })
 export class AppComponent {}
