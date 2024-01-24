491895454a14364d036fdb1cf2e9ab950092d61e
Renato <renamedrj@gmail.com>
2024-01-23 23:57:54 -0300

    Class 23. Property binding

--- meu-primeiro-projeto-latest/src/app/components/template/template-binding/template-binding.component.ts
+++ meu-primeiro-projeto-latest/src/app/components/template/template-binding/template-binding.component.ts
@@ -11,4 +11,12 @@ export class TemplateBindingComponent {
   public name: string = 'Renato Medeiros';
   public age: number = Math.abs(new Date(Date.now() - new Date("06/11/1987").getTime()).getUTCFullYear() - 1970);
   public condition : string = this.age > 1 ? "Teste" : "Teste 2";
+  public isDisabled : boolean = true;
+  public srcValue: string = "https://images.prismic.io/blogadevindev/58129589-de25-4254-99a9-507859e01db4_angular-logo.png";
+
+  constructor() {
+    setTimeout(() => {
+      this.name= 'Renato'
+    }, 1000);
+  }
 }
