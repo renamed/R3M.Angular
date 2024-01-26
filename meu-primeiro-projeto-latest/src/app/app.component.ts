6fd26b9205eb99330927a0899f9e72737507c5ec
Renato <renamedrj@gmail.com>
2024-01-26 17:34:35 -0300

    Class 31. @IF

--- meu-primeiro-projeto-latest/src/app/app.component.ts
+++ meu-primeiro-projeto-latest/src/app/app.component.ts
@@ -6,15 +6,17 @@ import { RouterOutlet } from '@angular/router';
 import { NewComponent } from './components/new-component/new-component.component';
 import { TemplateBindingComponent } from './components/template/template-binding/template-binding.component';
 import { TemplateVariablesComponent } from "./components/template/template-variables/template-variables.component";
+import { TemplateControlFlowComponent } from "./components/template/template-control-flow/template-control-flow.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     template: `
     <h1>Curso de Angular</h1>
-    <app-template-binding />
-    <app-template-variables />
+    <!-- <app-template-binding />
+    <app-template-variables /> -->
+    <app-template-control-flow />
     `,
-    imports: [CommonModule, RouterOutlet, NewComponent, TemplateBindingComponent, TemplateVariablesComponent]
+    imports: [CommonModule, RouterOutlet, NewComponent, TemplateBindingComponent, TemplateVariablesComponent, TemplateControlFlowComponent]
 })
 export class AppComponent {}
---
+++ meu-primeiro-projeto-latest/src/app/components/template/template-control-flow/template-control-flow.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-template-control-flow',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './template-control-flow.component.html',
+  styleUrl: './template-control-flow.component.scss'
+})
+export class TemplateControlFlowComponent {
+  public isTrue = false;
+}
