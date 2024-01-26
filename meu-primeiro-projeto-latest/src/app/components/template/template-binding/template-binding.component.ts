49096864d5db948898f7765c0e93d1c3db970d28
Renato <renamedrj@gmail.com>
2024-01-26 10:57:57 -0300

    Class 26. Event binding

--- meu-primeiro-projeto-latest/src/app/components/template/template-binding/template-binding.component.ts
+++ meu-primeiro-projeto-latest/src/app/components/template/template-binding/template-binding.component.ts
@@ -9,15 +9,32 @@ import { Component } from '@angular/core';
 })
 export class TemplateBindingComponent {
   public name: string = 'Renato Medeiros';
-  public age: number = Math.abs(new Date(Date.now() - new Date("06/11/1987").getTime()).getUTCFullYear() - 1970);
+  public beginningAge: number = Math.abs(new Date(Date.now() - new Date("06/11/1987").getTime()).getUTCFullYear() - 1970);
+  public age: number = this.beginningAge;
   public condition : string = this.age > 1 ? "Teste" : "Teste 2";
   public isDisabled : boolean = true;
   public srcValue: string = "https://images.prismic.io/blogadevindev/58129589-de25-4254-99a9-507859e01db4_angular-logo.png";
   public isTextDecoration: string = this.age >= 37 ? 'underline' : 'none';
 
-  constructor() {
-    setTimeout(() => {
-      this.name= 'Renato'
-    }, 1000);
+  // constructor() {
+  //   setTimeout(() => {
+  //     this.name= 'Renato'
+  //   }, 1000);
+  // }
+
+  public sum() {
+    this.age++;
+  }
+
+  public sub() {
+    this.age--;
+  }
+
+  public onKeyDown(event:Event) {
+    console.log(event)
+  }
+
+  public onMouseMove(event: MouseEvent) {
+    console.log(event)
   }
 }
