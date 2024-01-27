c4f9774171500d6d27003da0c5ceea91b79a0d95
Renato <renamedrj@gmail.com>
2024-01-26 22:21:17 -0300

    Class 39. @defer - when

--- meu-primeiro-projeto-latest/src/app/components/template/template-deferrable-views/template-deferrable-views.component.ts
+++ meu-primeiro-projeto-latest/src/app/components/template/template-deferrable-views/template-deferrable-views.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
 import { NewComponent } from "../../new-component/new-component.component";
+import { Observable, delay, of } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
     selector: 'app-template-deferrable-views',
     standalone: true,
     templateUrl: './template-deferrable-views.component.html',
     styleUrl: './template-deferrable-views.component.scss',
-    imports: [NewComponent]
+    imports: [NewComponent, CommonModule]
 })
 export class TemplateDeferrableViewsComponent {
-  public mostrar = false;
+  // public mostrar = false;
+  public isTrue = true;
+  public isTrue2 = false;
+
+  public loadingData$: Observable<string[]> = of([
+    'Item 1',
+    'Item 2',
+    'Item 3'
+  ]).pipe(delay(13000));
 }
